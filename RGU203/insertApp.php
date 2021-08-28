d4d2804bebe89570780ce5334fab1d832c64300a
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-28 03:46:22 -0700

    uo

--- RGU203/insertApp.php
+++ RGU203/insertApp.php
@@ -42,11 +42,9 @@ if(isset($_POST["import"])){
     $Change = mysqli_real_escape_string($connect, $worksheet->getCellByColumnAndRow(6,$row)->getValue());
   
  //VALUES("'.$Date.'","'.$Price.'","'.$High.'","'.$Open.'","'.$Low.'","'.$Vol.'","'.$Change.'")"; 
-    echo $Date;
-    
 
      $query = "INSERT INTO cba_data(Date_f,Price_f,High_f,Open_f,Low_f,Vol_f,Change_f)
-     VALUES('.$Date.','.$Price.','.$High.','.$Open.','.$Low.','.$Vol.','.$Change.')"; 
+     VALUES('$Date','$Price','$High','$Open','$Low','$Vol','$Change')"; 
     
     $res=mysqli_query($connect, $query); 
 
