f56cf90afdfad70d9081d688ff82445d617f1c3e
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-28 12:36:39 -0700

    yp

--- ctable.php
+++ ctable.php
@@ -23,9 +23,7 @@
             "ASXBHP VARCHAR(40) NOT NULL, ", 
             "ASXCSL DATE, "
          );
-         $sql = "CREATE TABLE tut_tbl( ".
-            "id INT NOT NULL AUTO_INCREMENT, ".
-            $fls.";
+         $sql = "CREATE TABLE tut_tbl($fls)";
          if ($mysqli->query($sql)) {
             printf("Table tutorials_tbl created successfully.<br />");
          }
