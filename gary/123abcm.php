2dc6d7cd135a92c4b98ec41508f58e39eb2bd6e4
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-30 13:04:32 -0700

    update

--- gary/123abcm.php
+++ gary/123abcm.php
@@ -5,13 +5,27 @@ $nms=explode(',',$names );
 
 $n=count($nms);
 
-for($i=0;$i<$n;$i++){
+for($i=0;$i<5;$i++){
+//for($i=0;$i<$n;$i++){
   
   //echo trim($nms[$i])."<br/>";
 
  $file = fopen((trim($nms[$i])),"r");
 while(! feof($file)){
-  print_r(fgetcsv($file));
+  
+  //print_r(fgetcsv($file));
+  
+    $data=fgetcsv($file);
+    $k=count($data);
+
+    print_r($data[0])."<br/>";
+    print_r($data[1])."<br/>";
+    print_r($data[2])."<br/>";
+    print_r($data[3])."<br/>";
+    print_r($data[4])."<br/>";
+    print_r($data[5])."<br/>";
+
+
 }
 fclose($file);
 
