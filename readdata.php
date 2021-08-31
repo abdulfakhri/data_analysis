a05ebaf140b1468436547346da56fb24def9d223
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-31 11:42:02 -0700

    up

--- readdata.php
+++ readdata.php
@@ -24,6 +24,8 @@ if (mysqli_num_rows($result) > 0) {
 
         //$ar = explode("<br>",$row["file_contents"]);
         //echo $ar[0]."<br>";
+        $companyCodes=str_replace("garyData/","",$row["filename"]);
+        echo $companyCodes."<br>";
 
         
     }
