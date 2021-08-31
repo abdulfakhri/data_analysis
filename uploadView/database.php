4b0ff8e25ca4953decd4d7007a92d56a6f35d604
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-30 21:39:46 -0700

    u

---
+++ uploadView/database.php
@@ -0,0 +1,13 @@
+<?php 
+$hostname     = "localhost";
+$username     = "u587940520_gray";
+$password     = "!@#123qweasdZXC"; 
+$databasename = "u587940520_gary"; 
+
+// Create connection 
+$conn = new mysqli($hostname, $username, $password,$databasename);
+ // Check connection 
+if ($conn->connect_error) { 
+die("Unable to Connect database: " . $conn->connect_error);
+ }
+?>
