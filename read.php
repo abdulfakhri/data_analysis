8919124d53ba4465a4378fd27e32796a002a66aa
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-31 16:17:21 -0700

    up

--- read.php
+++ read.php
@@ -36,7 +36,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM alldata LIMIT 10";
+$sql = "SELECT * FROM alldata LIMIT 100";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 // output data of each row
