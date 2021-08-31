e3cc77473b705f60d3a01714910ee53761996015
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-31 14:04:38 -0700

    up

--- read.php
+++ read.php
@@ -25,3 +25,44 @@ tr:nth-child(even) {background-color: #f2f2f2}
 <th>Username</th>
 <th>Password</th>
 </tr>
+<?php
+$servername = "localhost";
+$username = "u587940520_gray";
+$password = "!@#123qweasdZXC";
+$dbname = "u587940520_gary";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT * FROM alldata";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    $row = mysqli_fetch_assoc($result);
+    $n=count($row);
+    while($row) {
+       // echo "id: " . $row["ID"]. " - Name: " . $row["filename"]. " " . $row["file_contents"]. "<br>";
+        //$ar = explode("<br>",$row["file_contents"]);
+        //echo $ar[0]."<br>";
+        $companyCodes=str_replace("garyData/","",$row["filename"]);
+        $companyCodes=str_replace(".csv","",$companyCodes);
+        //echo trim($companyCodes)."<br>";
+        $companyCodes=trim($companyCodes);  
+
+        echo "<tr><td>" . $row["ID"]. "</td><td>" . $row["filename"] . "</td><td>". $row["file_contents"]. "</td></tr>";
+
+
+
+
+
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($conn);
+?>
+</table>
+</body>
+</html>
--- readdata.php
+++ readdata.php
@@ -13,35 +13,24 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 $sql = "SELECT * FROM alldata";
 $result = mysqli_query($conn, $sql);
-
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     $row = mysqli_fetch_assoc($result);
     $n=count($row);
-
-   
     while($row) {
         echo "id: " . $row["ID"]. " - Name: " . $row["filename"]. " " . $row["file_contents"]. "<br>";
-
         //$ar = explode("<br>",$row["file_contents"]);
         //echo $ar[0]."<br>";
         $companyCodes=str_replace("garyData/","",$row["filename"]);
         $companyCodes=str_replace(".csv","",$companyCodes);
         //echo trim($companyCodes)."<br>";
-        $companyCodes=trim($companyCodes);
-        
-
-        
+        $companyCodes=trim($companyCodes);  
     }
-   
-
 } else {
     echo "0 results";
 }
-
 mysqli_close($conn);
 ?>
 
