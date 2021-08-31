bd0b2af93487d2f09501a90ce06d3cf247a7402c
Abdul Fakhri <abf.ca@hotmail.com>
2021-08-31 11:08:54 -0700

    up

--- readdata.php
+++ readdata.php
@@ -20,7 +20,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "id: " . $row["ID"]. " - Name: " . $row["filename"]. " " . $row["file_contents"]. "<br>";
     }
 } else {
     echo "0 results";
