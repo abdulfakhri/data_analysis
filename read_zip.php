c33931e5752c4e211cd795545b4c932619400d58
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-01 10:44:13 -0700

    a

--- read_zip.php
+++ read_zip.php
@@ -14,5 +14,25 @@ if ($zipfile) {
 
 }
 
+function readContents($zipfile){
+
+if ($zipfile) {
+  while ($zip_entry = zip_read($zipfile)) {
+    echo "<p>Name: " . zip_entry_name($zip_entry) . "<br>";
+    // Open directory entry for reading
+    if (zip_entry_open($zipfile, $zip_entry)) {
+      echo "File Contents:<br>";
+      // Read open directory entry
+      $contents = zip_entry_read($zip_entry);
+      echo "$contents<br>";
+      zip_entry_close($zip_entry);
+    }
+  echo "</p>";
+  }
+zip_close($zipfile);
+} 
+
+readContents($zip);
+
 readName($zip);
 
