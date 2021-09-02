71c027734afb815b6478f6f25cf08dc1c6bee7b6
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-02 01:01:50 -0700

    up

--- export.php
+++ export.php
@@ -114,7 +114,7 @@ $result = $statement->fetchAll();
       {
        echo '
        <tr>
-        <td>'.$i++.'</td>
+        <td>'."No".'</td>
         <td>'.$row["H_date"].'</td>
         <td>'.$row["PriceClose"].'</td>
         <td>'.$row["Volume"].'</td>
