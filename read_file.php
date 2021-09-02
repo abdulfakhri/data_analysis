8e63e0d2505bbc3693eaf179fcbf6affb98966e8
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-01 18:56:12 -0700

    up

---
+++ read_file.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Table with database</title>
+<style>
+table {
+border-collapse: collapse;
+width: 100%;
+color: #588c7e;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
+<body>
+<table>
+<tr>
+<th>No</th>
+<th>Date</th>
+<th>Close</th>
+<th>Volume</th>
+<th>CompanyCode</th>
+<th>Share Issues</th>
+<th>Market Capt</th>
+</tr>
+<?php
+include_once 'database.php';
+
+$sql = "SELECT * FROM companydata LIMIT 10";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+// output data of each row
+$cr=1;
+$day=1;
+while($row = $result->fetch_assoc()) {
+
+$rowData=explode(",",$row["file_contents"]);  
+
+$date=str_replace("Volume","",$rowData[1);
+$code=str_replace("newd/","",$row["filename"]);
+$code=str_replace(".csv","",$code);
+//$volume=explode(" ",$rowData[10]);
+$vol=strchr($rowData[10]," ");
+$volume=str_replace("$vol"," ",$rowData[10]);
+
+$dateCr=date_create("$rowData[1]");
+$date=date_format($dateCr,"Y-m-d");
+
+echo "<tr>";
+echo "<td>".$cr++."</td>";
+echo "<td>".$date."</td>";
+echo "<td>".$rowData[9]."</td>";
+echo "<td>".$volume."</td>";
+echo "<td>".$rowData[0]."</td>";
+echo "<td>"."N/A"."</td>";
+echo "<td>"."N/A"."</td>";
+echo "</tr>";
+
+}
+
+} else { 
+    echo "0 results"; 
+}
+$conn->close();
+?>
+</table>
+</body>
+</html>
