446cad34c2352c55244ace004b67a26c95105b2a
Abdul Fakhri <abf.ca@hotmail.com>
2021-09-12 19:07:12 -0700

    up

--- printf.php
+++ printf.php
@@ -88,7 +88,7 @@ $volume=round($volume,2);
  }
 }
 
-$query = "SELECT * FROM alldata LIMIT 1000 ";
+$query = "SELECT * FROM alldata LIMIT 10 ";
 
 $statement = $connect->prepare($query);
 $statement->execute();
